3f65b556be8a4abafcf54dbcdc2e6539ecd08053
diego <diegofreeman78@gmail.com>
2025-07-04 06:14:27 +0100

    feat: add react-query and network connectivity management
    - Add @tanstack/react-query for state management
    - Implement network connectivity monitoring with @react-native-community/netinfo
    - Update VSCode server configuration port

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,15 @@
 import { useColorScheme } from '@/hooks/useColorScheme';
+import NetInfo from '@react-native-community/netinfo';
 import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
 } from '@react-navigation/native';
+import {
+  onlineManager,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
@@ -11,8 +17,13 @@ import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
+const queryClient = new QueryClient();
 SplashScreen.preventAutoHideAsync();
-
+onlineManager.setEventListener((setOnline) => {
+  return NetInfo.addEventListener((state) => {
+    setOnline(!!state.isConnected);
+  });
+});
 // Set the animation options. This is optional.
 SplashScreen.setOptions({
   duration: 1000,
@@ -40,14 +51,16 @@ export default function RootLayout() {
   const isLoggedIn = true;
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Protected guard={isLoggedIn}>
-          <Stack.Screen name="(protected)" options={{ headerShown: false }} />
-        </Stack.Protected>
-        <Stack.Protected guard={!isLoggedIn}>
-          <Stack.Screen name="(public)" options={{ headerShown: false }} />
-        </Stack.Protected>
-      </Stack>
+      <QueryClientProvider client={queryClient}>
+        <Stack>
+          <Stack.Protected guard={isLoggedIn}>
+            <Stack.Screen name="(protected)" options={{ headerShown: false }} />
+          </Stack.Protected>
+          <Stack.Protected guard={!isLoggedIn}>
+            <Stack.Screen name="(public)" options={{ headerShown: false }} />
+          </Stack.Protected>
+        </Stack>
+      </QueryClientProvider>
       <StatusBar style="auto" />
     </ThemeProvider>
   );
