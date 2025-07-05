ac8428ed580defc15e32a3ff86f04353a0e0b7b6
diego <diegofreeman78@gmail.com>
2025-07-05 04:36:48 +0100

    feat(tabs): add new tab screens and update layout
    Add new tab screens for Performance, Assignments, and More. Remove old Explore screen and update tab layout configuration. Also update tint color to use shared constants.

--- app/(protected)/(tabs)/_layout.tsx
+++ app/(protected)/(tabs)/_layout.tsx
@@ -2,10 +2,10 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
 
+import { Colors } from '@/constants/Colors';
 import { HapticTab } from '@/features/shared/components/HapticTab';
 import { IconSymbol } from '@/features/shared/components/ui/IconSymbol';
 import TabBarBackground from '@/features/shared/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function TabLayout() {
@@ -25,19 +25,43 @@ export default function TabLayout() {
           },
           default: {},
         }),
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="performance"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Performance',
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="chart.dots.scatter" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="assignments"
+        options={{
+          title: 'Assignments',
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="list.bullet.clipboard" color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="more"
+        options={{
+          title: 'More',
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="ellipsis.circle.fill" color={color} />
+          ),
         }}
       />
     </Tabs>
---
+++ app/(protected)/(tabs)/performance.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const PerformanceScreen = () => {
+  return (
+    <View>
+      <Text>PerformanceScreen</Text>
+    </View>
+  );
+};
+
+export default PerformanceScreen;
